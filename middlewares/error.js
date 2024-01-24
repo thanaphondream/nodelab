bd83ca5088c97de69dfb92f659c13325b6c55c26
unknown <voigkgky@gmail.com>
2024-01-24 15:46:05 +0700

    first commit

--- middlewares/error.js
+++
@@ -1,8 +0,0 @@
-const errorHandler = (err, req, res, next) => {
-  console.log(err);
-  res
-    .status(err.statusCode || 500)
-    .json({ message: err?.message || "Internal server error" });
-};
-
-module.exports = errorHandler;
--- middlewares/not-found.js
+++
@@ -1,5 +0,0 @@
-const notFoundHandler = (req, res, next) => {
-  res.status(404).json({ message: "resource not found on this server" });
-};
-
-module.exports = notFoundHandler;
